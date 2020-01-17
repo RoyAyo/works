fb911c6e8a6a4b172b21f25b496b098c824fa2d6
RoyAyo <roylayindeay0@gmail.com>
2020-01-17 16:24:30 +0100

    initial commit

---
+++ index.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+#standards for all outputs
+standard_ph_max = 8.5
+standard_ph_min = 6.5
+standard_turbidity_max = 3.5
+standard_turbidity_min = 3.0
+cell_volume = 60000 #in L
+cell_pressure = 5 #in bar
+
