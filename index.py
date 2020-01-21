a78cbc1106b43b1ff85ac37637ab91744771328e
RoyAyo <roylayindeay0@gmail.com>
2020-01-21 11:35:53 +0100

    added the sedimentation tank

--- index.py
+++ index.py
@@ -1,10 +1,42 @@
 import numpy as np
 
 #standards for all outputs
-standard_ph_max = 8.5
-standard_ph_min = 6.5
-standard_turbidity_max = 3.5
-standard_turbidity_min = 3.0
-cell_volume = 60000 #in L
-cell_pressure = 5 #in bar
 
+
+class Program():
+	self.standard_ph_max = 8.5
+	self.standard_ph_min = 6.5
+	self.standard_turbidity_max = 3.5
+	self.standard_turbidity_min = 3.0
+	self.cell_volume = 60000 #in L
+	self.cell_pressure = 5 #in bar
+
+	def __init__(flow_rate,turbidity,pH):
+		self.flow_rate = flow_rate
+		self.turbidity = turbidity
+		self.pH = pH
+		workflow()
+
+	def workflow():
+		while true:
+			(self.flow_rate,self.turbidity,self.pH) = chemical_tank(self.flow_rate,self.turbidity,self.pH)
+			
+
+	def chemical_tank(flow_rate,turbidity,pH):
+		self.chlorine = 0.2
+
+
+	def cells(inlet vol):
+		#the sensor checking the turbidity
+
+		while true:
+			sensor_turbidity = sensor.
+		return 0
+
+
+	def sedimentation(turbidity):
+		while true:
+			pass
+		return 0
+
+	def chlorinate(pH):
